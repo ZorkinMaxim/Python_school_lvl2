eb03ab55750bbb6395da4d0987c2970cbaa1100a
ZorkinMaxim <74238953+ZorkinMaxim@users.noreply.github.com>
2021-06-05 23:06:47 +0300

    Add files via upload

--- 3_proxy_pattern_OOP/jsonstorage.py
+++ 3_proxy_pattern_OOP/jsonstorage.py
@@ -3,13 +3,13 @@ import json
 
 class JSONStorage:
     def __init__(self):
-        self.name = "data.txt"
+        self.name = "data.json"
 
     def save(self, data):
-        with open('data.txt', 'w') as f:
+        with open('data.json', 'w') as f:
             json.dump(data, f)
 
     def load(self):
-        with open('data.txt', 'r') as json_file:
+        with open('data.json', 'r') as json_file:
             data = json.load(json_file)
         return data
--- 3_proxy_pattern_OOP/update.py
+++ 3_proxy_pattern_OOP/update.py
@@ -2,7 +2,7 @@ import json
 
 new = {"siblings":4, "work":"IT park"}
 
-with open('data.txt', 'r+') as f:
+with open('data.json', 'r+') as f:
     x = json.load(f)
     x.update(new)
     f.seek(0)
